f008605f57824ff6f75b6ab3e708f544cda80492
20001LastOrder <boqi.chen@mail.mcgill.ca>
2020-02-21 23:56:36 -0500

    add drop table statements, reorganize files

--- statements/create_table.sql
+++ statements/create_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE Users(
-	email VARCHAR(100),
+  email VARCHAR(100),
   uName VARCHAR(30),
   phoneNumber VARCHAR(15),
   uAddress VARCHAR(100),
@@ -7,13 +7,13 @@ CREATE TABLE Users(
 );
 
 CREATE TABLE Institutions(
-	iName VARCHAR(100),
-    iAddress VARCHAR(100),
-	PRIMARY KEY(iName)
+  iName VARCHAR(100),
+  iAddress VARCHAR(100),
+  PRIMARY KEY(iName)
 );
 
 CREATE TABLE Patrons(
-	email VARCHAR(100),
+  email VARCHAR(100),
   maxBooks INTEGER,
   iName VARCHAR(100) NOT NULL,
   PRIMARY KEY(email),
@@ -26,30 +26,30 @@ CREATE TABLE Libraries
   lName varchar(100),
   iName varchar(100),
   lAddress varchar(100),
-  PRIMARY KEY(lName)
+  PRIMARY KEY(lName,iName),
+    FOREIGN KEY(iName) REFERENCES Institutions
 );
 
 CREATE TABLE Librarians(
-	email VARCHAR(100),
+  email VARCHAR(100),
   workingDays VARCHAR(7),
   iName VARCHAR(100) NOT NULL,
   lName VARCHAR(100) NOT NULL,
   PRIMARY KEY(email),
   FOREIGN KEY(email) REFERENCES Users,
-  FOREIGN KEY(iName) REFERENCES Institutions,
-  FOREIGN KEY(lName) REFERENCES Libraries
+  FOREIGN KEY(lName,iName) REFERENCES Libraries
 );
 
 CREATE TABLE Administrators(
   email VARCHAR(100),
-	iName VARCHAR(100),
+  iName VARCHAR(100),
   PRIMARY KEY(email),
   FOREIGN KEY(email) REFERENCES Users,
   FOREIGN KEY(iName) REFERENCES Institutions
 );
 
 CREATE TABLE Books(
-	isbnNumber VARCHAR(15),
+  isbnNumber VARCHAR(15),
   title VARCHAR(30),
   category VARCHAR(20),
   publicationDate DATE,
@@ -59,19 +59,18 @@ CREATE TABLE Books(
 );
 
 CREATE TABLE Authors(
-	authorId INTEGER,
+  authorId INTEGER,
   aName VARCHAR(100),
   PRIMARY KEY(authorId)
 );
 
 CREATE TABLE BookCopies(
-	barCode VARCHAR(50),
+  barCode VARCHAR(50),
   lName VARCHAR(100) NOT NULL,
   iName VARCHAR(100) NOT NULL,
   isbnNumber VARCHAR(15) NOT NULL,
   PRIMARY KEY(barCode),
-  FOREIGN KEY(lName) REFERENCES Libraries,
-  FOREIGN KEY(iName) REFERENCES Institutions,
+  FOREIGN KEY(lName,iName) REFERENCES Libraries,
   FOREIGN KEY(isbnNumber) REFERENCES Books
 );
 
---
+++ statements/drop_table.sql
@@ -0,0 +1,32 @@
+DROP Table Requests;
+
+DROP TABLE Writes;
+
+DROP Table Loans;
+
+DROP TABLE BookCopies;
+
+DROP TABLE Authors;
+
+DROP TABLE Books;
+
+DROP TABLE Administrators;
+
+DROP TABLE Librarians;
+
+DROP TABLE Libraries;
+
+DROP TABLE Patrons;
+
+DROP TABLE Institutions;
+
+DROP TABLE users;
+
+
+
+
+
+
+
+
+
