e2a1e0d836b5f631b32f7b6c27596ffc3f45f4c3
20001LastOrder <boqi.chen@mail.mcgill.ca>
2020-02-26 21:01:01 -0500

    add count query

---
+++ statements/count.sql
@@ -0,0 +1,12 @@
+SELECT COUNT(*) as AuthorCount From Authors;
+SELECT COUNT(*) as BookCopyCount From BookCopies;
+SELECT COUNT(*) as BookCount From Books;
+SELECT COUNT(*) as InstitutionCount From Institutions;
+SELECT COUNT(*) as LibraryCount From Libraries;
+SELECT COUNT(*) as LibrarianCount From Librarians;
+SELECT COUNT(*) as LoanCount From Loans;
+SELECT COUNT(*) as AdministratorCount From Administrators;
+SELECT COUNT(*) as PatronCount From Patrons;
+SELECT COUNT(*) as UserCount From Users;
+SELECT COUNT(*) as RequestCount From Requests;
+SELECT COUNT(*) as WriteCount From Writes;
